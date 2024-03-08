374c3fb738aac1b8948dd4485c9558a34ac1c6c2
Raheem Lawal <lawalraheem@gmail.com>
2024-03-08 01:06:02 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,152 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className='text-left font-bold text-4xl'>RAHEEM LAWAL</div>
+      <div className='mb-4 text-left font-bold text-xl'>ENGINEER</div>
+      <div className='mb-4 font-semibold text-md'>PORTFOLIO</div>
+      <div className="relative overflow-x-auto rounded-lg">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 rounded-lg">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                      <th scope="col" className="px-6 py-3">
+                          Application Name
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          Category
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          Technologies
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          Description
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          Links
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr className=" bg-slate-100  dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-black text-gray-900 whitespace-nowrap">
+                          HeemToken
+                      </th>
+                      <td className="px-6 py-4 text-black">
+                          Web
+                      </td>
+                      <td className="px-6 py-4">
+                        <span className="me-1 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">React</span>
+                        <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Firebase</span>
+                      </td>
+                      <td className="px-6 py-4 text-black">
+                          Cryptocurrency deployed on Binance Smart Chain
+                      </td>
+                      <td className="px-6 py-4">
+                        <button className="bg-gray-700 p-1 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                            <g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" fill="currentColor" /></g>
+                          </svg>
+                        </button>
+                      </td>
+                  </tr>
+                  <tr className=" bg-slate-100 dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-black text-gray-900 whitespace-nowrap">
+                          3D Pineapple
+                      </th>
+                      <td className="px-6 py-4 text-black">
+                          Mobile
+                      </td>
+                      <td className="px-6 py-4">
+                        <span className="me-1inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Swift</span>
+                        <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Reality Composer</span>
+                      </td>
+                      <td className="px-6 py-4 text-black">
+                          Augmented reality application for Apple Vision Pro
+                      </td>
+                      <td className="px-6 py-4">
+                        <button className="bg-gray-700 p-1 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                            <g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" fill="currentColor" /></g>
+                          </svg>
+                        </button>
+                      </td>
+                  </tr>
+                  <tr className="border-radius-1 bg-slate-100 dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-black text-gray-900 whitespace-nowrap">
+                          Cleanse
+                      </th>
+                      <td className="px-6 py-4 text-black">
+                          Web
+                      </td>
+                      <td className="px-6 py-4">
+                        <span className="me-1 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Leo</span>
+                        <span className="me-1 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">React</span>
+                        <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Tailwind</span>
+                      </td>
+                      <td className="px-6 py-4 text-black">
+                          Zero knowledge app to privately transfer funds on Aleo
+                      </td>
+                      <td className="px-6 py-4">
+                        <button className="bg-gray-700 p-1 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                            <g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" fill="currentColor" /></g>
+                          </svg>
+                        </button>
+                      </td>
+                  </tr>
+                  <tr className="border-radius-1 bg-slate-100 dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-black text-gray-900 whitespace-nowrap">
+                          Landlorde
+                      </th>
+                      <td className="px-6 py-4 text-black">
+                          Web
+                      </td>
+                      <td className="px-6 py-4">
+                        <span className="me-1 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">React</span>
+                        <span className="me-1 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Firebase</span>
+                        <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">ChakraUI</span>
+                      </td>
+                      <td className="px-6 py-4 text-black">
+                          Real estate management platform for individuals
+                      </td>
+                      <td className="px-6 py-4">
+                        <button className="bg-gray-700 p-1 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                            <g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" fill="currentColor" /></g>
+                          </svg>
+                        </button>
+                      </td>
+                  </tr>
+                  <tr className="border-radius-1 bg-slate-100 dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-black text-gray-900 whitespace-nowrap">
+                          Dummy
+                      </th>
+                      <td className="px-6 py-4 text-black">
+                          Web + Mobile
+                      </td>
+                      <td className="px-6 py-4">
+                        <span className="me-1 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">FastAPI</span>
+                        <span className="me-1 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Railway</span>
+                        <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Gemini</span>
+                      </td>
+                      <td className="px-6 py-4 text-black">
+                          Rest API with sample endpoints
+                      </td>
+                      <td className="px-6 py-4">
+                        <button className="bg-gray-700 p-1 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                            <g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" fill="currentColor" /></g>
+                          </svg>
+                        </button>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+    </div>
+  )
+}
+
+export default App
